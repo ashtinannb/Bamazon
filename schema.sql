bca706ed04a9907615716e0ab36df163eb1887b8
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-05-21 15:34:41 -0500

    created table and columns

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+    item_id datatype,
+    product_name datatype,
+    department_name datatype,
+    price datatype,
+    stock_quantity datatype,
+);
