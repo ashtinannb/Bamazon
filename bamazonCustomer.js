4ae68e98e5a502a07b59d88fd3ceb8e5e05111a6
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-05-21 21:09:49 -0500

    finished load products function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,24 +3,50 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 
+
+
+
+
+
 // initialize connection variable
 var connection = mysql.createConnection({
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "1755",
-    database: "bamazon"
+  host: "127.0.0.1",
+  port: 3306,
+  user: "root",
+  password: "1755",
+  database: "bamazon"
 });
 
 
-// connect to mysql 
-connection.query("SELECT * FROM products", function (err, res) {
-    if (err) throw err;
 
+
+// variable connects with the server, and then in turn loads the products 
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+  }
+  loadProducts();
+});
+
+
+
+
+
+// load products function
+function loadProducts() {
+  // Selects all of the data from the MySQL products table
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
     // creates table in terminal, so freaking cool 
     console.table(res);
-
-    // asking customer what they would like 
+    // asking customer what they would like
     promptCustomer(res);
-});
+  });
+}
+
+
+
+
+
+
 
