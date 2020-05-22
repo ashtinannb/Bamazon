598f3837cfeaf1d3707ec84eea18e29850b5ec36
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-05-21 22:47:52 -0500

    functions for asking customer and product quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,11 +3,6 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 
-
-
-
-
-
 // initialize connection variable
 var connection = mysql.createConnection({
   host: "127.0.0.1",
@@ -16,8 +11,6 @@ var connection = mysql.createConnection({
   password: "1755",
   database: "bamazon"
 });
-
-
 
 
 // variable connects with the server, and then in turn loads the products 
@@ -29,12 +22,11 @@ connection.connect(function (err) {
 });
 
 
+////// all my functions 
 
-
-
-// load products function
+/// function for loading products 
 function loadProducts() {
-  // Selects all of the data from the MySQL products table
+  // grabs data from my database
   connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
     // creates table in terminal, so freaking cool 
@@ -45,6 +37,72 @@ function loadProducts() {
 }
 
 
+
+
+
+
+/// function for asking customer 
+function promptCustomer(inventory) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "choice",
+        message: "Hello! What would you like to purchase today? [Quit with Q]",
+        validate: function (val) {
+          return !isNaN(val) || val.toLowerCase() === "q";
+        }
+      }
+    ])
+    .then(function (val) {
+      Exit(val.choice);
+      var choiceId = parseInt(val.choice);
+      var product = checkInventory(choiceId, inventory);
+
+      if (product) {
+        promptQuantity(product);
+      }
+      else {
+        console.log("\nI'm sorry, we are out of that item.");
+        // re-run load products 
+        loadProducts();
+      }
+    });
+}
+
+
+
+
+
+
+// function for product quantity 
+function promptQuantity(product) {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "quantity",
+        message: "How many would you like? [Quit with Q]",
+        validate: function(val) {
+          return val > 0 || val.toLowerCase() === "q";
+        }
+      }
+    ])
+    .then(function(val) {
+      Exit(val.quantity);
+      var quantity = parseInt(val.quantity);
+
+      // not enough product??
+      if (quantity > product.stock_quantity) {
+        console.log("\nInsufficient quantity!");
+        loadProducts();
+      }
+      else {
+        //run buy, give it the product info and quantity 
+        buy(product, quantity);
+      }
+    });
+}
 
 
 
