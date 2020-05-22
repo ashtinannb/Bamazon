44b5e1e6711575615f3efa8a620974ae7f2646e4
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-05-21 19:40:46 -0500

    established connection to mysql variable

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,14 @@
 // initialize npms
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-require("console.table");
 
 
+// initialize connection variable
+var connection = mysql.createConnection({
+    host: "127.0.0.1",
+    port: 3306,
+    user: "root",
+    password: "1755",
+    database: "bamazon"
+});
 
